bd78c98e8ca3fb0598f278e7fcdd958b4117799e
Aquartic <r.martyniuk008@gmail.com>
2023-06-21 19:49:06 +0200

    додав класи полей

--- oop.py
+++ oop.py
@@ -1,22 +1,29 @@
 
 
 class Ship:
-    def __init__(self, surf, rect, coords):
+    def __init__(self, surf, rect, coords, size):
         self.surf = surf
         self.rect = rect
         self.coords = coords
-
-
-class Two_sized_ship(Ship):
-
-    def __init__(self, surf, rect, coords, size=2):
-
-        super().__init__(surf, rect, coords)
         self.size = size
 
-parentShip = Ship(4, 5, 6)
-ship = Two_sized_ship(1, 2, 3, 6)
+class Board:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+    def area_calc(self):
+        return self.height * self.width
 
-print(ship.size)
-print(parentShip.size)
+class Playerboard(Board):
+    def __init__(self, height, width, display):
 
+        super().__init__(height, width)
+        self.display = display
+class Ai(Board):
+    def __init__(self, height, width, surface):
+        super().__init__(height, width)
+        self.surface = surface
+player_board = Playerboard(10, 10, None)
+AI_board = Ai(10, 10, None)
+print(player_board.area_calc())
+print(AI_board.area_calc())
